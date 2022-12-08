c57180916fdefc03c85ecc2d537d0abfc7c71e7b
Rasmus Steen <rasmus@steen.ninja>
2022-12-08 06:41:34 +0100

    start day 8

--- AOC2022/Challenges/Challenge08/Challenge08.cs
+++ AOC2022/Challenges/Challenge08/Challenge08.cs
@@ -4,7 +4,27 @@ public class Challenge08 : IChallenge
 {
     public async Task<string> Part1(InputReader reader)
     {
-        throw new PartNotImplementedException(1);
+        var trees = new List<List<int>>();
+
+        await foreach (var line in reader.ReadAllLinesAsync())
+        {
+            trees.Add(line.Select(x => (int)char.GetNumericValue(x)).ToList());
+        }
+
+        var currentHighsTop = trees.First();
+        var currentHighsBot = trees.Last();
+        var result = currentHighsTop.Count + currentHighsBot.Count;
+
+        for (int i = 1; i < trees.Count; i++)
+        {
+            result += 2;
+
+            var row = trees[i];
+            var currentHighLeft = row.First();
+            var currentHighRight = row.Last();
+
+        }
+
     }
 
     public async Task<string> Part2(InputReader reader)
